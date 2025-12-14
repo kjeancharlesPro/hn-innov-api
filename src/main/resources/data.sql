3718c4dc4fa1876ae8938991f1078263a46df704
kjeancharlesPro <kjeancharles@hn-services.fr>
2025-12-15 04:07:06 +0700

    update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,8 @@ insert into status (id, state) values (1, 'EN_ATTENTE');
 
 insert into period (id, start_date, end_date) values (1, '2025-11-20 00:00:00','2025-11-25 00:00:00');
 
-insert into participants(id, first_name, last_name, email, skill) values (100L, 'Jon', 'Doe', 'jdoe@mail.com', 'Développeur');
-insert into participants(id, first_name, last_name, email, skill) values (101L, 'James', 'Rocket', 'jamrodket@mail.com', 'Développeur');
-insert into participants(id, first_name, last_name, email, skill) values (102L, 'Jessy', 'Rocket', 'jessrocket@mail.com', 'Développeur');
-insert into participants(id, first_name, last_name, email, skill) values (103L, 'Jo', 'Jo', 'jojo@mail.com', 'Développeur');
-
-insert into jury_member(id, first_name, last_name, email, title, description, problem, innovation) values (1, 'Pre', 'Pos','mail', 'title', 'description','problem', 'innov' );
+--insert into participants(id, first_name, last_name, email, skill) values (100L, 'Jon', 'Doe', 'jdoe@mail.com', 'Développeur');
+--insert into participants(id, first_name, last_name, email, skill) values (101L, 'James', 'Rocket', 'jamrodket@mail.com', 'Développeur');
+--insert into participants(id, first_name, last_name, email, skill) values (102L, 'Jessy', 'Rocket', 'jessrocket@mail.com', 'Développeur');
+--insert into participants(id, first_name, last_name, email, skill) values (103L, 'Jo', 'Jo', 'jojo@mail.com', 'Développeur');
+--insert into jury_member(id, first_name, last_name, email, title, description, problem, innovation) values (1, 'Pre', 'Pos','mail', 'title', 'description','problem', 'innov' );
